aa07067e28a3feb07e74ce8f50169438b292ddab
AnnaMatveenko-qa <annamatveenko85@gmail.com>
2023-12-01 19:25:13 +0200

    practice removed

--- src/main/java/matveenko/lesson9/DivisionByZeroException.java
+++
@@ -1,8 +0,0 @@
-package matveenko.lesson9;
-
-public class DivisionByZeroException extends ArithmeticException{
-    public DivisionByZeroException(String message) {
-        super(message);
-    }
-
-}
